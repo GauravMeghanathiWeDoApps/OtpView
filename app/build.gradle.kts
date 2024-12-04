c3afd36a6fe844f327771a7ed0c81f6ae7d002fa
GauravMeghanathiWeDoApps <117021255+GauravMeghanathiWeDoApps@users.noreply.github.com>
2024-12-04 11:38:25 +0530

    otpView Jitpack file added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+//    implementation(libs.gauravmeghanathiwedoapps.otpview)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
