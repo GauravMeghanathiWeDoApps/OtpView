0bc1de13b2274e66ad22e2c14336bdfdbdd23511
GauravMeghanathiWeDoApps <117021255+GauravMeghanathiWeDoApps@users.noreply.github.com>
2024-12-04 12:30:12 +0530

    version change 1.0.1

--- OTPView/build.gradle.kts
+++ OTPView/build.gradle.kts
@@ -41,7 +41,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.mg"
             artifactId = "otpview"
-            version = "1.0.0"
+            version = "1.0.1"
             afterEvaluate{
                 from(components["release"])
             }
